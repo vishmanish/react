cc36e96f6f27ae643a14e7ca62dec3500dd26d77
vishmanish <88487626+vishmanish@users.noreply.github.com>
2021-09-08 18:53:54 +0530

    react prac1

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Login from './login'
+import Car from './car'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <Login/>
+      <Car/>
+    </div>
+  );
+}
+
+export default App;
